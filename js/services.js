29cd7038cdbf36b39c67239ecc38d7bd77242b52
Dmytro Lylo <lylodmyt@fel.cvut.cz>
2022-08-11 15:52:32 +0200

    commit

--- js/services.js
+++ js/services.js
@@ -2,14 +2,16 @@ $('.responsive').slick({
     dots: true,
     infinite: true,
     speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    cssEase: 'linear',
+    mobileFirst: true,
     responsive: [
       {
-        breakpoint: 1400,
+        breakpoint: 768,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 2,
+          slidesToScroll: 1,
           infinite: true,
           dots: true
         }
@@ -17,17 +19,11 @@ $('.responsive').slick({
       {
         breakpoint: 1050,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 565,
-        settings: {
-          slidesToShow: 1,
+          slidesToShow: 3,
           slidesToScroll: 1
         }
       }
+      
       // You can unslick at a given breakpoint now by adding:
       // settings: "unslick"
       // instead of a settings object
