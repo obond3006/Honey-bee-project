be1ae2db00256ac92d0a4da5a64b89d0188fbbec
m-litovka <michael.litovka@gmail.com>
2022-08-11 16:22:28 +0000

    Mike: added slider v. 0.1

--- js/services.js
+++ js/services.js
@@ -29,3 +29,34 @@ $('.responsive').slick({
       // instead of a settings object
     ]
   });
+
+  $('.responsiveStore').slick({
+    infinite: true,
+    speed: 300,
+    mobileFirst: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    cssEase: 'linear',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1050,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1
+        }
+      }
+      
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
