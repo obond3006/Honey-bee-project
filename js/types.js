e6a2287b1b943cc9458033406b35a259210b0205
LysVolodymyr <lysvol29@gmail.com>
2022-08-15 11:09:59 +0000

    slider for types

--- js/types.js
+++ js/types.js
@@ -1,6 +1,31 @@
 $(document).ready(function(){
-    $('.slider_types').slick({
-        dots: true,
-        slidesToShow: 1,
-    });
-});
+    $('.types-carusel').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  mobileFirst: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 922,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+    
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+    })
+ });
+    
