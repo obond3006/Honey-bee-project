2a908e69134667380ad87b5f59c08f6904614f4d
Olha <obond3006@gmail.com>
2022-08-28 22:17:01 +0000

    Edited accordion opening

--- js/faq.js
+++ js/faq.js
@@ -4,15 +4,20 @@
   
     for (let i = 0; i < accordions.length; i++) {
         accordions[i].addEventListener('click', function () {
-            
-            this.classList.toggle("is-open");
-  
+
             const content = this.nextElementSibling;
-  
+            
             if (content.style.maxHeight) {
                 content.style.maxHeight = null;
-                } else {
-                content.style.maxHeight = content.scrollHeight + "px";
+                this.classList.remove("is-open");
+            } else {
+                let active = document.querySelectorAll(".accordion-label.is-open");
+                for(let j = 0; j < active.length; j++) {
+                    active[j].classList.remove("is-open");
+                    active[j].nextElementSibling.style.maxHeight = null;
+                }
+            this.classList.toggle("is-open");
+            content.style.maxHeight = content.scrollHeight + "px";
             }
         })
     }
