27a2504f58c54408b9fdda0f68f1ed18c6bba036
Dmytro Lylo <59778411+lylodmyt@users.noreply.github.com>
2022-08-31 15:59:05 +0200

    Update cart-popup.js

--- js/cart-popup.js
+++ js/cart-popup.js
@@ -1,17 +1,24 @@
-const MODAL_ACTIVE_CLASS_NAME = 'modal-active';
+(function(){
+    const MODAL_ACTIVE_CLASS_NAME = 'modal-active';
 
-const formModal = document.querySelector('#form-modal');
-
-const openFormModalBtn = document.querySelector('.cart__button');
-const launchBtn = document.querySelector('#launch-btn');
-const closeBtns = document.querySelectorAll('.close-btn');
-
-openFormModalBtn.addEventListener('click', (event) => {
-console.log(event)
-    formModal.classList.add(MODAL_ACTIVE_CLASS_NAME);
-})
-
-const closeFormModal = () => {
-    formModal.classList.remove(MODAL_ACTIVE_CLASS_NAME);
-};
+    const formModal = document.querySelector('#form-modal');
+    
+    const openFormModalBtn = document.querySelector('.cart__button');
+    const launchBtn = document.querySelector('#launch-btn');
+    const closeBtns = document.querySelector('.close-btn');
+    
+    openFormModalBtn.addEventListener('click', (event) => {
+    console.log(event)
+        formModal.classList.add(MODAL_ACTIVE_CLASS_NAME);
+    })
+    
+    const closeFormModal = () => {
+        formModal.classList.remove(MODAL_ACTIVE_CLASS_NAME);
+    };
+    
+    closeBtns.addEventListener('click', (event) => {
+        console.log('close');
+        formModal.classList.remove(MODAL_ACTIVE_CLASS_NAME);
+    });
+})();
 
